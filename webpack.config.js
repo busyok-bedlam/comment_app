cd9c47a30767e9b174d64890a960e5f223118838
Bulat Cherkashin <busyokbedlam@gmail.com>
2018-12-14 15:12:29 +0200

    pre eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,15 @@
+const path = require('path');
 const config = {
-
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+        ]
+      }
 };
 
 module.exports = config;
