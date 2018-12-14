24e9a78ee2249d9b8fe8855e94f470722985a2b7
Bulat Cherkashin <busyokbedlam@gmail.com>
2018-12-14 17:25:38 +0200

    complete eslint settings

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const rootElement = window.document.getElementById('root');
+const App = () => (<div>HELLO</div>);
+ReactDOM.render(<App />, rootElement);
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = {
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-          { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
-        ]
-      }
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    }],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'App',
+      template: path.resolve(__dirname, 'src', 'template.ejs'),
+    }),
+  ],
 };
 
 module.exports = config;
